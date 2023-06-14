626c9de4117b452a4147fc59650b961af59b2aab
azulcuellar1 <112140643+azulcuellar1@users.noreply.github.com>
2023-06-13 20:39:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Mi primer proyecto en la consola");
+        //establece un nombre y un apellido dentro de las comillas
+            var primer_nombre = "Camila";
+            var apellido = "    díaz";
+
+            var nombre_completo = primer_nombre.concat(apellido);
+            //imprime en consola la variable nombre completo
+            console.log(nombre_completo);
+
+            var a = 10;
+            var b = 5;
+            //establece la suma de a + b;
+            var sumar = a+b
+            console.log(sumar);
+
+            var restar = a - b ;
+            console.log(restar);
+            //multiplica las variables a * b;
+            var multiplicar = a *b
+            console.log(multiplicar);
+
+            //imprime en la consola la variable dividir
+            var dividir = a / b ;
+            console.log(dividir);
+            
